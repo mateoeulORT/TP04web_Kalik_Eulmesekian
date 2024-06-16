c3357b5d971a959c965bb9eaa36db4b6b84e606a
mateoeulORT <48044168@est.ort.edu.ar>
2024-06-16 19:56:24 -0300

    arreglar input e inicializar el diccionario

--- Models/OrtWorld.cs
+++ Models/OrtWorld.cs
@@ -3,7 +3,7 @@ class OrtWorld{
     public static List <string> ListaHoteles {get; private set;} = new List<string>{"imagen1.jpg", "imagen2.jpg", "imagen3.jpg", "imagen4.jpg", "imagen5.jpg", "imagen6.jpg", "imagen7.jpg","imagen8.jpg", "imagen9.jpg", "imagen10.jpg"};
     public static List<string> ListaAereos {get; private set;} = new List<string>{"flybondi.jpg", "lufthansa.jpg", "Aereolineas Argentinas.jpg", "LAN.jpg", "Qatar airways.jpg", "Fly emirates.jpg", "iberia.jpg","BOA.jpg", "Air France.jpg", "air belgium.jpg"};
     public static List<string> ListaExcursiones {get; private set;} = new List<string>{"excu1.jpg", "excu2.jpg", "excu3.jpg", "excu4.jpg", "excu5.jpg", "excu6.jpg", "excu7.jpg","excu8.jpg", "excu9.jpg", "excu10.jpg"};
-    public static Dictionary<string, Paquete> DicPaquetes {get; private set;}
+    public static Dictionary<string, Paquete> DicPaquetes { get; private set; } = new Dictionary<string, Paquete>();
 
    public static bool IngresarPaquete(string destinoSeleccionado, Paquete paquete){
     
--- Models/Paquete.cs
+++ Models/Paquete.cs
@@ -2,7 +2,7 @@ class Paquete{
     public string Hotel {get; private set;}
     public string Aereo {get; private set;}
    public string Excursion {get; private set;}
-
+    public Paquete(){}
     public Paquete(string hotel, string aereo, string excursion){
         Hotel = hotel;
         Aereo = aereo;
